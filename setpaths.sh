21ceeceecf5d4550fd0b74385f9cf75eed51ba97
Irene Perez Gonzalez <iregon@noc.ac.uk>
2020-06-26 00:26:35 +0100

    Removed scracth

--- setpaths.sh
+++ setpaths.sh
@@ -5,13 +5,3 @@ mug_code_directory=$code_directory/marine-user-guide
 
 echo 'Data directory is '$data_directory
 echo 'User guide code directory is '$mug_code_directory
-
-# Create the scratch directory for the user
-scratch_directory=/work/scratch-nopw/$(whoami)/user-manual
-if [ ! -d $scratch_directory ]
-then
-  echo "Creating user $(whoami) scratch directory $scratch_directory"
-  mkdir $scratch_directory
-else
-  echo "Scratch directory is $scratch_directory"
-fi
